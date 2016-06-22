b288689eba6de96457f8682684183c4389be39a2
David Huff <David.Huff@computer-critters.com>
2016-06-22 08:15:33 -0400

    trying to get redis-store working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,9 +56,10 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  # NOTE: ENV vars aren't available during slug comiplation, so must check if they exist:
+  # NOTE: ENV vars aren't available during slug compilation, so must check if they exist:
   if ENV["REDISCLOUD_URL"]
-    config.cache_store = :redis_store, ENV["REDISCLOUD_URL"], { expires_in: 90.minutes }
+    # config.cache_store = :redis_store, ENV["REDISCLOUD_URL"], { expires_in: 90.minutes }
+    config.cache_store = :redis_store, ENV["REDISCLOUD_URL"]
   end
 
 
